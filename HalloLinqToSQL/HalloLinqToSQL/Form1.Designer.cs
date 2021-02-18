65d96606439803c9bf31f717d3106c1bb0de10d8
André <ruland@outlook.de>
2021-02-18 12:13:48 +0100

    Linq-To-Sql veraltet aber eigentlich gut

---
+++ HalloLinqToSQL/HalloLinqToSQL/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace HalloLinqToSQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.BurlyWood;
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Controls.Add(this.button4);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(985, 41);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load all Employees";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button2.Location = new System.Drawing.Point(183, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(59, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button3.Location = new System.Drawing.Point(248, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(57, 35);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "New";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button4.Location = new System.Drawing.Point(311, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(142, 35);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Load all Orders";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(985, 492);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            this.dataGridView1.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dataGridView1_CellFormatting);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(985, 533);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ HalloLinqToSQL/HalloLinqToSQL/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Data;
+using System.Data.Linq;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace HalloLinqToSQL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //DataClasses1DataContext context = new DataClasses1DataContext();//nicht so lange leben lassen wie das Programmm läuft
+        DataClasses1DataContext context = null;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            context = new DataClasses1DataContext(); //Unit-of-Work wird mit den laden gestartet (cache dauer von LazyLoading)
+
+            var query = context.Employees
+                               .Where(x => x.FirstName.Length > 3)
+                               .OrderBy(x => x.BirthDate)
+                               .Take(100);
+
+            Debug.WriteLine(query.ToString());
+
+            dataGridView1.DataSource = query.ToList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            context.SubmitChanges();
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.CurrentRow.DataBoundItem is Employee emp)
+            {
+                MessageBox.Show($"{emp.LastName}");
+                //MessageBox.Show($"{emp.LastName} {string.Join(", ", emp.Orders.Select(x => x.OrderDate.Value.ToString("d")))}"); //wenn order == DbNull, dann NullRef Exception
+
+                var orderDates = string.Join(", ", emp.Orders.Where(x => x.OrderDate.HasValue).Select(x => x.OrderDate.Value.ToString("d")));
+                MessageBox.Show($"{emp.LastName} {orderDates}");
+                //foreach (var order in emp.Orders)
+                //{
+                //    MessageBox.Show(order.OrderDate.ToString());
+                //}
+            }
+        }
+
+        //public  Nullable<int> NullableInt { get; set; }
+        public int? NullableInt { get; set; }
+
+        public DateTime Datum { get; set; } = DateTime.MinValue;
+        public int Zahl { get; set; }
+        public bool Bool { get; set; }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var newEmp = new Employee()
+            {
+                FirstName = "Wilma",
+                LastName = "Feuerstein",
+                BirthDate = DateTime.Now.AddYears(-30)
+            };
+
+            context.Employees.InsertOnSubmit(newEmp);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            context = new DataClasses1DataContext();
+            //context.DeferredLoadingEnabled = false; //lazy loading deaktivieren
+            
+            var op = new DataLoadOptions();         //eager Loading
+            op.LoadWith<Order>(x => x.Employee);    //eager Loading
+            op.LoadWith<Order>(x => x.Customer);    //eager Loading
+            op.LoadWith<Order>(x => x.Shipper);     //eager Loading
+            context.LoadOptions = op;               //eager Loading
+            dataGridView1.DataSource = context.Orders.ToList();
+        }
+
+        private void dataGridView1_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (e.Value is Employee emp)
+            {
+                e.Value = $"{emp.FirstName} {emp.LastName}";
+            }
+        }
+    }
+}
