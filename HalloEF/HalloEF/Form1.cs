4d6126b7be203c509f4b0fc65c0931a4fda2a140
André <ruland@outlook.de>
2021-02-18 12:39:03 +0100

    EF mit Designer (Db First)

---
+++ HalloEF/HalloEF/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+
+namespace HalloEF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var context = new NORTHWNDEntities();
+
+            dataGridView1.DataSource = context.Orders.Include(x=>x.Employee).OrderBy(x => x.OrderDate).ToList();
+        }
+    }
+}
